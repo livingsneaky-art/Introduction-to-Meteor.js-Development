780eac63844017e4b94f914f1fd9e3f3a9cb994e
livingsneaky-art <ryanllamido@gmail.com>
2023-03-25 10:17:02 +0800

    Create week_1.js

---
+++ week_1.js
@@ -0,0 +1,29 @@
+//Introduction to Meteor.js Development course overview
+//Guidance for the prerequisite quiz
+
+//Prerequisite quize
+
+//Overview
+
+//Course outline
+
+//Grading and assessment information
+
+//Why meteor?
+
+
+//Introduction
+//Overview
+
+//URL for this module
+
+//Code for this module
+
+
+//From one to many users
+
+
+//Install Meteor
+//URL for this lession
+
+//Install Meteor
