5d16aebf76cd780052b666dbc5059a9603b07291
livingsneaky-art <ryanllamido@gmail.com>
2023-03-29 13:27:35 +0800

    Partial notes of week 2

---
+++ week_2.js
@@ -0,0 +1,21 @@
+//Databases anc collections
+//Introduction
+
+//Overview
+
+//URL for this module
+
+//Code for this module
+
+
+//Meteor distributed data model
+
+
+//Create a collection of images
+
+
+//Better start up script, removing items from a collection
+
+
+
+
