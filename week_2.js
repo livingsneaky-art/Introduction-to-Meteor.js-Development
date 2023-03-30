90ddb44c4048e7d21598be4cb4a6f92378623711
livingsneaky-art <ryanllamido@gmail.com>
2023-03-30 20:55:31 +0800

    Update week_2.js

--- week_2.js
+++ week_2.js
@@ -17,5 +17,22 @@
 //Better start up script, removing items from a collection
 
 
+//Add an image rating function: Updating and sorting
+
+//URL for this lesson
 
 
+//Implement image adding with a Bootstrap Modal
+
+//Implement image adding with a Bootstrap modal(part 1)
+
+//Implement image adding with a Bootstrap modal(part 2)
+
+
+//Databases and collections summary
+
+//Databases and collections summary quiz
+
+//Siteace part 1: edit an event and a template
+
+//Siteace part 1: edit an event and a template
