a9384e8970002cec41e0994ba27377f355699d81
livingsneaky-art <ryanllamido@gmail.com>
2023-03-31 00:20:54 +0800

    Partial week 3 notes

---
+++ week_3.js
@@ -0,0 +1,40 @@
+//User authentication
+//introduction
+
+//Overview
+
+//Code for this module
+
+//User authentication with Meteor.
+
+
+//Tidying up the design wit a navbar
+
+
+//Accessing user information
+
+//URL for this lesson
+
+
+//Customising the user registration form
+
+//URL for this lesson
+
+
+//Attaching users to images
+
+
+
+
+//Filtering images by user
+
+//Filtering images by user
+
+
+//Removing the image filter
+
+
+//Infinite scroll
+
+
+//User authentication summary
